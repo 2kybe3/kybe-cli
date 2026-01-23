b885d804156dec6d94456835a343e37f4e7ed048
2kybe3 <kybe@kybe.xyz>
2026-01-23 17:54:22 +0100

    add more commands

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,83 @@
 mod config;
 
-use std::sync::Arc;
+use std::{io, sync::Arc};
 
 use anyhow::Context;
-use clap::Parser;
+use clap::{Command, CommandFactory, Parser, Subcommand};
+use clap_complete::{Generator, Shell, generate};
 use parking_lot::Mutex;
 
 use crate::config::types::Config;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Allows you to edit the config with your prefered editor
-    #[arg(long)]
-    edit_config: bool,
+#[command(version, long_about = None, name = "kcli")]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(subcommand_required = true, arg_required_else_help = true)]
+    Config {
+        #[command(subcommand)]
+        command: Option<GenerateCompletionsCommands>,
+    },
+    GenerateCompletions {
+        #[arg(short, long)]
+        shell: Shell,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum GenerateCompletionsCommands {
+    Show,
+    Edit,
+}
+
+fn print_completions<G: Generator>(generator: G, cmd: &mut Command) {
+    generate(
+        generator,
+        cmd,
+        cmd.get_name().to_string(),
+        &mut io::stdout(),
+    );
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+    let cli = Cli::parse();
 
     let cfg = Arc::new(Mutex::new(Config::load()?));
 
-    if args.edit_config {
-        let cfg_guard = cfg.lock();
+    if let Some(Commands::GenerateCompletions { shell }) = cli.command {
+        let mut cmd = Cli::command();
+        eprintln!("Generating completion file for {shell:?}...");
+        print_completions(shell, &mut cmd);
+        std::process::exit(0);
+    }
 
-        let editor = std::env::var("EDITOR").unwrap_or_else(|_| "vi".into());
+    if let Some(Commands::Config { command }) = cli.command {
+        match command {
+            Some(GenerateCompletionsCommands::Edit) => {
+                let cfg_guard = cfg.lock();
 
-        let status = std::process::Command::new(editor)
-            .arg(&cfg_guard.user_file)
-            .status()
-            .context("failed to open editor")?;
+                let editor = std::env::var("EDITOR").unwrap_or_else(|_| "vi".into());
 
-        if !status.success() {
-            eprintln!("Editor exited with non-zero status");
+                let status = std::process::Command::new(editor)
+                    .arg(&cfg_guard.user_file)
+                    .status()
+                    .context("failed to open editor")?;
+
+                if !status.success() {
+                    eprintln!("Editor exited with non-zero status");
+                }
+            }
+            Some(GenerateCompletionsCommands::Show) => {
+                let cfg_guard = cfg.lock();
+
+                println!("{:#?}", *cfg_guard);
+            }
+            None => {}
         }
 
         std::process::exit(0);
@@ -51,7 +96,6 @@ fn main() -> anyhow::Result<()> {
     {
         let mut cfg_lock = cfg.lock();
         cfg_lock.generated.last_launch = Some(chrono::Utc::now());
-        println!("{:?}", *cfg_lock);
     }
 
     // 2. parse the argument -> decides what action to run
